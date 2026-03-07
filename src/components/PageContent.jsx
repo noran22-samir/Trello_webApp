1653d9d54210ac83237d958a2ec32a797aac4ddb
Shahd Adel <shahdadelsaed@gmail.com>
2026-03-07 05:33:16 +0200

    merge-with-main

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -63,7 +63,7 @@ const PageContent = () => {
         <div className="workspace-header">
           <span id="last-title">Trello Workspace</span>
           <div className="workspace-buttons">
-            <button className="last-btns">Views</button>
+            <button className="last-btns">Views</button>   
             <button className="last-btns">Members</button>
             <button className="last-btns">Settings</button>
             <button className="last-btns">Upgrade</button>
