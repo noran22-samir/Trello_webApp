9b029cb9284a5ee3e36e43a2423e09cac3539ca2
M1g0x <migo.ee0@gmail.com>
2026-03-09 23:30:09 +0200

    profile & settings

--- src/components/sideMenuList.jsx
+++ src/components/sideMenuList.jsx
@@ -1,38 +1,52 @@
-import { Menu} from "antd";
+import { Menu } from "antd";
 import {
   SettingOutlined,
   TeamOutlined,
-    AppstoreOutlined,
-    CalendarOutlined, BellOutlined,InfoCircleOutlined,
-    
-  
+  AppstoreOutlined,
+  CalendarOutlined,
+  BellOutlined,
+  InfoCircleOutlined,
   TableOutlined,
 } from "@ant-design/icons";
-
+import ProfileVisibility from "./Profile";
+import WorkspaceSettings from "./Settings";
 
 const SideMenuList = () => {
-    return (
-      <Menu className='sidemenu'>
-        <Menu.Item key="home" icon={<AppstoreOutlined />}>
-          
-        </Menu.Item>
+  return (
+    <Menu className='sidemenu' mode="inline">
+      <Menu.Item key="home" icon={<AppstoreOutlined />}>
+        Home
+      </Menu.Item>
 
-        <Menu.Item key="team" icon={<TeamOutlined />}>
-          
-        </Menu.Item>
-        <Menu.Item key="settings" icon={<SettingOutlined />}>
-          
-        </Menu.Item>
-        <Menu.Item key="calender" icon={<CalendarOutlined />}>
-          
-        </Menu.Item>
-        <Menu.Item key="notfications" icon={<BellOutlined />}>
-          
-        </Menu.Item>
-        <Menu.Item key="info" icon={<InfoCircleOutlined />}>
-          
-        </Menu.Item>
-      </Menu>
-    );
-}
+      <Menu.Item key="team" icon={<TeamOutlined />}>
+        Team
+      </Menu.Item>
+
+      <Menu.Item key="settings" icon={<SettingOutlined />}>
+        Settings
+      </Menu.Item>
+
+      <Menu.Item key="calendar" icon={<CalendarOutlined />}>
+        Calendar
+      </Menu.Item>
+
+      <Menu.Item key="notifications" icon={<BellOutlined />}>
+        Notifications
+      </Menu.Item>
+
+      <Menu.Item key="info" icon={<InfoCircleOutlined />}>
+        Info
+      </Menu.Item>
+
+      <Menu.Item key="profile" icon={<ProfileVisibility />}>
+        Profile
+      </Menu.Item>
+
+      <Menu.Item key="workspace_settings" icon={<WorkspaceSettings />}>
+        Workspace
+      </Menu.Item>
+    </Menu>
+  ); 
+};
+
 export default SideMenuList;
